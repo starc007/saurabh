151df0821eca864fbe4ac4c1ae5b3f538b98485e
Saurabh <infinityincorporative51@gmail.com>
2026-01-19 23:27:41 +0530

    chore: migrate project to Next.js, update dependencies, and clean up unused files

--- src/components/Tabs/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Tabs } from "./tabs.main";
-export { default as LineTabs } from "./tabsLine.main";
--- src/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-export enum WORK_TYPE_TABS {
-  ALL = "All",
-  WEB3 = "Web3",
-  OPEN_SOURCE = "Open Source",
-  FREELANCE = "Freelance",
-  WEB2 = "Web2",
-}
